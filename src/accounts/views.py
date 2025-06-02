cfbfa2311bbfb299fe0ec2a41463bea72896d58d
Konstantin <halcov12@gmail.com>
2025-06-02 14:24:32 +0300

    Added Index, Product, Profile

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -13,7 +13,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect("wms:index")  # редирект после успешного входа
+            return redirect("wms:index")
     else:
         form = AuthenticationForm()
     return render(request, "accounts/login.html", {"form": form})
