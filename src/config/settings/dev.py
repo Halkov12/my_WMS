6c5a7aae7d14c9ca808b97547201d84c6bc6520f
Konstantin <halcov12@gmail.com>
2025-06-14 12:10:53 +0300

    Added docker-compose

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from config.settings.base import *  # NOQA:F403
 
 SECRET_KEY = "django-insecure-mp3m1$h1($o)*wekb!t2=y$b^u)16t8gt)m6r$xbyxnx647l(4"
@@ -15,15 +19,14 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",  # NOQA:F405
     },
-    "default":{
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
         "PORT": os.environ.get("POSTGRES_PORT"),
-
-    }
+    },
 }
 
 STATIC_URL = "/static/"
