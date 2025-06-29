419eb35679b8433e7ea223cce1e5e08575ed3fe1
Konstantin <halcov12@gmail.com>
2025-06-29 09:10:09 +0300

    Added nginx

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -4,5 +4,25 @@ from accounts.models import Customer
 
 
 @admin.register(Customer)
-class ProductAdmin(admin.ModelAdmin):
-    pass
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'role', 'is_active', 'date_joined']
+    list_filter = ['role', 'is_active', 'is_staff', 'date_joined']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-date_joined']
+    
+    fieldsets = (
+        ('Основна інформація', {
+            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'birth_date', 'photo', 'role')
+        }),
+        ('Робоча інформація', {
+            'fields': ('position', 'department')
+        }),
+        ('Права доступу', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Важливі дати', {
+            'fields': ('date_joined', 'last_login_date', 'total_logins', 'profile_views')
+        }),
+    )
+    
+    readonly_fields = ['date_joined', 'last_login_date', 'total_logins', 'profile_views']
